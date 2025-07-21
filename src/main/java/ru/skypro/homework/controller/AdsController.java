d0b4b60d98d62a36dd487719c87533f36882b056
sbakhareva <snezhana.bakhareva@yandex.ru>
2025-07-21 15:51:14 +0300

    добавлена документация JavaDoc в контроллеры

--- src/main/java/ru/skypro/homework/controller/AdsController.java
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -2,8 +2,6 @@ package ru.skypro.homework.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -11,14 +9,9 @@ import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.*;
 
 import org.springframework.http.MediaType;
-import ru.skypro.homework.model.AdImage;
 import ru.skypro.homework.service.AdImageService;
 import ru.skypro.homework.service.AdsService;
 import ru.skypro.homework.service.CommentsService;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 @RestController
 @AllArgsConstructor
@@ -30,12 +23,25 @@ public class AdsController {
     private final CommentsService commentsService;
     private final AdImageService adImageService;
 
+    /**
+     * Получение всех существующих в базе данных объявлений
+     *
+     * @return объект {@link Ads}, содержащий общее количество и список кратких ДТО объявлений {@link Ad}
+     */
     @GetMapping
     @Operation(summary = "Получение всех объявлений", tags = {"Объявления"})
     public Ads getAllAds() {
         return adsService.getAllAds();
     }
 
+    /**
+     * Добавление нового объявления
+     *
+     * @param createOrUpdateAd объект {@link CreateOrUpdateAd}, содержащий основную информацию об объявлении
+     * @param adImage          картинка в формате MultipartFile
+     * @param authentication   информация о пользователе из хидеров
+     * @return объект {@link Ad}, содержащий информацию об объявлении и id создавшего его пользователя
+     */
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "Добавление объявления", tags = {"Объявления"})
     public Ad addAd(@RequestPart("properties") CreateOrUpdateAd createOrUpdateAd,
@@ -45,7 +51,13 @@ public class AdsController {
         return adsService.addAd(createOrUpdateAd, adImage, email);
     }
 
-
+    /**
+     * Получение информации об объявлении
+     *
+     * @param id             идентификатор объявления
+     * @param authentication информация о пользователе из хидеров
+     * @return объект {@link ExtendedAd}, содержащий подробную информацию об объявлении и создавшем его пользователе
+     */
     @GetMapping("/{id}")
     @Operation(summary = "Получение информации об объявлении", tags = {"Объявления"})
     public ExtendedAd getAdInfo(@PathVariable("id") Integer id,
@@ -54,6 +66,16 @@ public class AdsController {
         return adsService.getAdInfo(id, email);
     }
 
+    /**
+     * Удаление объявления
+     *
+     * @param id             идентификатор объявления
+     * @param authentication информация о пользователе из хидеров
+     * @return {@link ResponseEntity}:
+     * <ul>
+     *   <li><b>204 No Content</b> – если объявление успешно удалено</li>
+     * </ul>
+     */
     @DeleteMapping("/{id}")
     @Operation(summary = "Удаление объявления", tags = {"Объявления"})
     public ResponseEntity<?> deleteAd(@PathVariable("id") Integer id,
@@ -63,6 +85,14 @@ public class AdsController {
         return ResponseEntity.noContent().build();
     }
 
+    /**
+     * Обновление информации об объявлении
+     *
+     * @param id             идентификатор объявления
+     * @param ad             объект {@link CreateOrUpdateAd}, содержащий данные для обновления информации объявления
+     * @param authentication информация о пользователе из хидеров
+     * @return объект {@link Ad}, содержащий информацию об объявлении и id создавшего его пользователя
+     */
     @PatchMapping("/{id}")
     @Operation(summary = "Обновление информации об объявлении", tags = {"Объявления"})
     public Ad updateAds(@PathVariable(value = "id", required = true) Integer id,
@@ -72,6 +102,12 @@ public class AdsController {
         return adsService.updateAd(id, ad, email);
     }
 
+    /**
+     * Получение всех объявлений авторизованного пользователя
+     *
+     * @param authentication информация о пользователе из хидеров
+     * @return объект {@link Ads}, содержащий общее количество и список кратких ДТО объявлений {@link Ad}
+     */
     @GetMapping("/me")
     @Operation(summary = "Получение объявлений авторизованного пользователя", tags = {"Объявления"})
     public Ads getAdsMe(Authentication authentication) {
@@ -79,6 +115,14 @@ public class AdsController {
         return adsService.getAdsMe(email);
     }
 
+    /**
+     * Обновление картинки объявления
+     *
+     * @param id             идентификатор объявления
+     * @param image          новая картинка
+     * @param authentication информация о пользователе из хидеров
+     * @return ссылка на картинку в файловой системе
+     */
     @PatchMapping("/{id}/image")
     @Operation(summary = "Обновление картинки объявления", tags = {"Объявления"})
     public String updateImage(@PathVariable("id") Integer id,
@@ -88,12 +132,27 @@ public class AdsController {
         return adsService.updateImage(id, image, email);
     }
 
+    /**
+     * Получение комментариев объявления
+     *
+     * @param id идентификатор объявления
+     * @return объект {@link Comments}, содержащий количество комментариев к объявлению
+     * и информацию о комментариях в формате ДТО {@link Comment}
+     */
     @GetMapping("/{id}/comments")
     @Operation(summary = "Получение комментариев объявления", tags = {"Комментарии"})
     public Comments getComments(@PathVariable(value = "id", required = true) Integer id) {
         return commentsService.getComments(id);
     }
 
+    /**
+     * Добавление комментария к объявлению
+     *
+     * @param id             идентификатор объявления
+     * @param comment        объект {@link CreateOrUpdateComment}, содержащий текст комментария
+     * @param authentication информация о пользователе из хидеров
+     * @return объект {@link Comment}, содержащий подробную информацию о комментарии и оставившем его пользователе
+     */
     @PostMapping("/{id}/comments")
     @Operation(summary = "Добавление комментария к объявлению", tags = {"Комментарии"})
     public Comment addComment(@PathVariable("id") Integer id,
@@ -103,6 +162,17 @@ public class AdsController {
         return commentsService.addComment(id, comment, email);
     }
 
+    /**
+     * Удаление комментария
+     *
+     * @param adId           идентификатор объявления
+     * @param commentId      идентификатор комментария
+     * @param authentication информация о пользователе из хидеров
+     * @return {@link ResponseEntity}:
+     * <ul>
+     *   <li><b>204 No Content</b> – если комментарий успешно удален</li>
+     * </ul>
+     */
     @DeleteMapping("/{adId}/comments/{commentId}")
     @Operation(summary = "Удаление комментария", tags = {"Комментарии"})
     public ResponseEntity<?> deleteComment(@PathVariable(value = "adId", required = true) Integer adId,
@@ -113,6 +183,15 @@ public class AdsController {
         return ResponseEntity.noContent().build();
     }
 
+    /**
+     * Обновление комментария
+     *
+     * @param adId           идентификатор объявления
+     * @param commentId      идентификатор комментария
+     * @param comment        объект {@link CreateOrUpdateComment}, содержащий обновленный текст комментария
+     * @param authentication информация о пользователе из хидеров
+     * @return объект {@link Comment}, содержащий подробную информацию о комментарии и оставившем его пользователе
+     */
     @PatchMapping("/{adId}/comments/{commentId}")
     @Operation(summary = "Обновление комментария", tags = {"Комментарии"})
     public Comment updateComment(@PathVariable(value = "adId") Integer adId,
@@ -123,14 +202,27 @@ public class AdsController {
         return commentsService.updateComment(adId, commentId, comment, email);
     }
 
+    /**
+     * Отображение картинок всех объявлений на главной странице и всех объявлений пользователя
+     *
+     * @param imageId идентификатор картинки в базе данных
+     * @return картинка в виде массива байт
+     */
     @GetMapping(value = "/ads-images/{id}", produces = {MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_GIF_VALUE, "image/*"})
     public ResponseEntity<byte[]> getImage(@PathVariable("id") Integer imageId) {
         return adImageService.getImage(imageId);
     }
 
+    /**
+     * Отображение картинки текущего открытого объявления
+     *
+     * @param adId    идентификатор объявления
+     * @param imageId идентификатор картинки
+     * @return картинка в виде массива байт
+     */
     @GetMapping(value = "/{adId}/ads-images/{imageId}", produces = {MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_GIF_VALUE, "image/*"})
     public ResponseEntity<byte[]> getThisImage(@PathVariable("adId") Integer adId,
-                                           @PathVariable("imageId") Integer imageId) {
+                                               @PathVariable("imageId") Integer imageId) {
         return adImageService.getThisAdImage(adId, imageId);
     }
 }
--- src/main/java/ru/skypro/homework/controller/UserController.java
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -2,10 +2,7 @@ package ru.skypro.homework.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
-import lombok.SneakyThrows;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,14 +12,8 @@ import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.NewPassword;
 import ru.skypro.homework.dto.UpdateUser;
 import ru.skypro.homework.dto.User;
-import ru.skypro.homework.model.UserImage;
 import ru.skypro.homework.service.UserImageService;
 import ru.skypro.homework.service.UserService;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.security.PrivateKey;
 
 @RestController
 @AllArgsConstructor
@@ -34,6 +25,17 @@ public class UserController {
     private final UserService userService;
     private final UserImageService userImageService;
 
+    /**
+     * Обновление пароля пользователя
+     *
+     * @param newPassword    объект {@link NewPassword}, содержащий текущий и новый пароли
+     * @param authentication информация о пользователе из хидеров
+     * @return {@link ResponseEntity}:
+     * <ul>
+     *   <li><b>200 OK</b> – если пароль успешно обновлён</li>
+     *   <li><b>401 Unauthorized</b> – если текущий пароль не совпадает</li>
+     * </ul>
+     */
     @PostMapping("/set_password")
     @Operation(summary = "Обновление пароля")
     public ResponseEntity<?> setPassword(@RequestBody NewPassword newPassword,
@@ -47,6 +49,12 @@ public class UserController {
         }
     }
 
+    /**
+     * Получение информации об авторизованном пользователе
+     *
+     * @param authentication информация о пользователе из хидеров
+     * @return объект {@link User}, содержащий основную информацию о пользователе
+     */
     @GetMapping("/me")
     @Operation(summary = "Получение информации об авторизованном пользователе")
     public User getUser(Authentication authentication) {
@@ -54,6 +62,13 @@ public class UserController {
         return userService.getUser(email);
     }
 
+    /**
+     * Обновление информации об авторизованном пользователе
+     *
+     * @param updateUser     объект {@link UpdateUser}, содержащий данные пользователя для обновления информации в базе данных
+     * @param authentication информация о пользователе из хидеров
+     * @return объект {@link UpdateUser}, содержащий обновленные данные пользователя
+     */
     @PatchMapping("/me")
     @Operation(summary = "Обновление информации об авторизованном пользователе")
     public UpdateUser updateUser(@RequestBody UpdateUser updateUser,
@@ -62,6 +77,17 @@ public class UserController {
         return userService.updateUser(updateUser, email);
     }
 
+    /**
+     * Обновление аватара авторизованного пользователя
+     *
+     * @param image          новое фото пользователя
+     * @param authentication информация о пользователе из хидеров
+     * @return {@link ResponseEntity}:
+     * <ul>
+     *   <li><b>200 OK</b> – если фото успешно обновлено</li>
+     *   <li><b>401 Unauthorized</b> – если пользователь не авторизован</li>
+     * </ul>
+     */
     @PatchMapping("/me/image")
     @Operation(summary = "Обновление аватара авторизованного пользователя")
     public ResponseEntity<?> updateUserImage(@RequestParam MultipartFile image,
@@ -74,6 +100,12 @@ public class UserController {
         }
     }
 
+    /**
+     * Получение аватара текущего пользователя
+     *
+     * @param id идентификатор фото пользователя
+     * @return картинка в виде массива байт
+     */
     @GetMapping(value = "/users-images/{id}", produces = {MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_GIF_VALUE, "image/*"})
     public ResponseEntity<byte[]> getImage(@PathVariable("id") Integer id) {
         return userImageService.getImage(id);
